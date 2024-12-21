a481028e9cad606a4e0104dc04832772ba618230
Kayob <kayobe5565@gmail.com>
2024-12-21 19:30:55 +0000

    Add karma.conf.js for testing

---
+++ karma.conf.js
@@ -0,0 +1,40 @@
+// karma.conf.js
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {
+        // you can add special jasmine options here
+        random: true,
+        seed: '4321'
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    restartOnFileChange: true,
+    browserNoActivityTimeout: 60000,
+    browserDisconnectTolerance: 3
+  });
+};
